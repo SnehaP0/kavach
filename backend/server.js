9480ba781b2a0cb95aab6933fab24fe95135594a
SnehaP0 <24051962@kiit.ac.in>
2026-06-04 02:58:03 +0530

    add all alert routes - CRUD, upvote, comments

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import { pool } from "./db.js";
 
 dotenv.config();
+const upvote_id=1;// for now till auth is established
 const app=express();
 const server = http.createServer(app);
 const port=process.env.PORT|| 5000;
@@ -46,6 +47,67 @@ app.post("/api/alerts", async (req,res)=>{
     res.json(qer.rows[0]);}
     catch(err){
         res.status(500).json({error:err.message});
+    }
+});
+app.get("/api/alerts/:id",async (req,res)=>{
+    try{
+        const i=req.params.id;
+        const qer=await pool.query('SELECT * FROM alerts WHERE id =$1',[i]);
+        if (qer.rows.length === 0) {
+        return res.status(404).json({
+        error: "Alert not found"
+      });
+    }
+        res.json(qer.rows[0]);
+    }
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+app.delete("/api/alerts/:id", async (req,res)=>{
+   try{ const i=req.params.id;
+    const qer=await pool.query('DELETE FROM alerts WHERE id=$1',[i]);
+    if(qer.rowCount === 0){
+        return res.status(404).json({error: "Alert not found"});
+    }
+    return res.status(200).json({
+        message:"succesful"
+    });
+    }
+    catch(err){
+        res.status(505).json({error:err.message});
+    }
+});
+
+app.patch("/api/alerts/:id/upvote", async (req,res)=>{
+    try{
+    const i=req.params.id;
+    const qr= await pool.query('SELECT * FROM upvotes WHERE user_id=$1 AND alert_id=$2',[upvote_id,i]);
+    if(qr.rowCount != 0){
+        await pool.query('DELETE FROM upvotes WHERE user_id=$1 AND alert_id=$2',[upvote_id,i]);
+        await pool.query('UPDATE alerts SET upvote_count=upvote_count-1 WHERE id=$1',[i]);
+    }
+    else{
+        await pool.query('INSERT INTO upvotes(user_id,alert_id) VALUES ($1,$2)',[upvote_id,i]);
+        await pool.query('UPDATE alerts SET upvote_count=upvote_count+1 WHERE id=$1',[i]);
+    }
+    res.json({message: "upvote updated"});
+}
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+app.post("/api/alerts/:id/comments", async (req,res)=>{
+    try{
+        const i=req.params.id;
+        const comm=req.body.text;
+        await pool.query('INSERT INTO comments(alert_id,user_id,text) VALUES ($1,$2,$3)',[i,upvote_id,comm]);
+        res.status(200).json({message:comm});
+        }
+    catch(err){
+        res.status(500).json({error: err.message});
     }
 });
 server.listen(port, () => {
